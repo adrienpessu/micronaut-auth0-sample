5b374d792f1f51fdbda0a863fd371f5ce0364562
Adrien Pessu <7055334+adrienpessu@users.noreply.github.com>
2022-09-28 18:06:00 +0200

    test: add Spock

---
+++ src/test/java/example/micronaut/MicronautAuth0Test.java
@@ -0,0 +1,46 @@
+package example.micronaut;
+
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.runtime.server.EmbeddedServer;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+import org.spockframework.util.Assert;
+import spock.lang.AutoCleanup;
+import spock.lang.Shared;
+
+import javax.validation.constraints.AssertTrue;
+
+@MicronautTest
+class MicronautAuth0Test {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Inject
+    @Client("/")
+    HttpClient client;
+
+    @Shared
+    @AutoCleanup
+    EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer.class);
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+    @Test
+    void testIntegration() {
+        String result = client.toBlocking().retrieve(HttpRequest.GET("/"), String.class);
+
+        Assert.that(result.contains("Just click on"));
+    }
+
+}
